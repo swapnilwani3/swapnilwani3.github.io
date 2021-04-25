b361f851bffe8efeafb47a84c783a40cb86f1efa
swapnil wani <48331283+swapnilwani3@users.noreply.github.com>
2021-04-26 01:08:08 +0530

    removed imports

--- src/components/data/projects_data.js
+++ src/components/data/projects_data.js
@@ -1,12 +1,5 @@
-import tweetme from '../../assets/projects_images/tweetme.PNG'
-import chatapp from '../../assets/projects_images/chatapp.JPG'
-import hardware_store from '../../assets/projects_images/hardware_store.jpg'
-import portfolio from '../../assets/projects_images/portfolio.JPG'
-import new_year from '../../assets/projects_images/new_year.jpg'
-import color_classification from '../../assets/projects_images/color_classification.jpg'
-import blinking from '../../assets/projects_images/blinking.JPG'
 import COVID from '../../assets/projects_images/COVID.jpg'
-import Dev_talks from '../../assets/projects_images/Dev_talks.jpg'
+
 
 const data_projects = [
     {
